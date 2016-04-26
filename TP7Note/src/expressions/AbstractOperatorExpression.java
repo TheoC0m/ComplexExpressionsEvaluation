790c34e529e8d139d3a76cf5dc1f999391febcac
theo <theo@linux-laptop>
2016-04-27 00:44:25 +0200

    z

---
+++ TP7Note/src/expressions/AbstractOperatorExpression.java
@@ -0,0 +1,13 @@
+package expressions;
+
+public abstract class AbstractOperatorExpression implements Expression{
+
+	protected Expression leftSide;
+	protected Expression rightSide;
+	
+	public AbstractOperatorExpression() {
+	}
+	
+	
+
+}
